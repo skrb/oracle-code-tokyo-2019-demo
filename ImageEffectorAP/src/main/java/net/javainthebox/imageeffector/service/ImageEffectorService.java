848faa82c990040c99be223e31135ab3bc072675
skrb <yuichi.sakuraba@gmail.com>
2019-04-26 16:08:00 +0900

    apply SoftFocusEffect

--- ImageEffectorAP/src/main/java/net/javainthebox/imageeffector/service/ImageEffectorService.java
+++ ImageEffectorAP/src/main/java/net/javainthebox/imageeffector/service/ImageEffectorService.java
@@ -4,39 +4,54 @@ import com.sun.net.httpserver.Headers;
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 import com.sun.net.httpserver.HttpServer;
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.InetSocketAddress;
 import java.util.List;
 import javax.imageio.ImageIO;
-
+import net.javainthebox.imageeffector.SoftFocusEffector;
 
 public class ImageEffectorService implements HttpHandler {
+    
     public void handle(HttpExchange t) throws IOException {
+
 	Headers headers = t.getRequestHeaders();
-
-	int length = Integer.parseInt(headers.get("Content-length").get(0));
-	System.out.println("Length: " + length);
-	String contentType = headers.get("Content-type").get(0);
-	System.out.println("ContentType: " + contentType);
+	var contentType = headers.get("Content-type").get(0);
+	var formatType = contentType.substring(contentType.lastIndexOf('/')+1);
 	
-        InputStream is = t.getRequestBody();
-	//	byte[] buffer = is.readAllBytes();
-	//	System.out.println("Buffer Size: " + buffer.length);
-
-	java.util.Arrays.stream(ImageIO.getReaderFormatNames()).forEach(System.out::println);
-	var image = ImageIO.read(is);
-	System.out.println("W: " + image.getWidth() + " H: " + image.getHeight());
+        var is = t.getRequestBody();
+	BufferedImage image = ImageIO.read(is);
+	var width = image.getWidth();
+	var height = image.getHeight();
 	is.close();
 
+	System.out.println("Image: " + image);
+
+	// Apply Soft Focusing
+	int[] pixels = image.getRGB​(0, 0, width, height,
+				    null, 0, width);
+	System.out.println("Get Pixel: " + pixels.length + " " + pixels[0]);
+	
+	int[] appliedPixels = SoftFocusEffector.softenVector(pixels, width, height);
+	System.out.println("Apply Pixel: " + appliedPixels[0]);
+	BufferedImage appliedImage = new BufferedImage(width, height,
+						       BufferedImage.TYPE_INT_ARGB);
+	appliedImage.setRGB(0, 0, width, height, appliedPixels, 0, width);
+	System.out.println("Applied: " + appliedImage);
+	
+	var baos = new ByteArrayOutputStream();
+	ImageIO.write(appliedImage, formatType, baos);
+	var buffer = baos.toByteArray();
+	System.out.println("Size: " + buffer.length);
+	
 	t.getResponseHeaders().add("Content-type", contentType);
-	t.sendResponseHeaders(200, length);
+	t.sendResponseHeaders(200, buffer.length);
 	
 	OutputStream os = t.getResponseBody();
-	ImageIO.write(image, "jpg", os);
-	
-	//	os.write(buffer);
+	os.write(buffer);
         os.close();
     }
 
---
+++ ImageEffectorAP/startService.sh
@@ -0,0 +1,1 @@
+java -cp target/ImageEffectorAP-1.0-SNAPSHOT.jar --add-modules jdk.incubator.vector net.javainthebox.imageeffector.service.ImageEffectorService
